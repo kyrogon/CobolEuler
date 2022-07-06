7271dc153d64fd9124366892c3ac5451808364be
Aaron Rogers <aaron.kyle.rogers@gmail.com>
2022-07-05 23:12:16 -0400

    simplify Euler2

--- Euler2.cbl
+++ Euler2.cbl
@@ -9,68 +9,60 @@
 000900     01 ws-testor        pic 9(12) value 600851475143.
 001000     01 ws-remainder     pic 9(12).
 001100     01 ws-prime-can     pic 9(12) value 2.
-001200     01 ws-current       pic 9(9).
-001300     01 ws-nprimes       pic 9(9) value 5.
-001400     01 ws-primes.
-001500         05 ws-prime     pic 9(12) occurs 100 times indexed by i. 
-001600 procedure division.
-001700 perform 100-init thru 100-exit.
-001800 goback.
-001900 100-init.
-002000     continue.
-002100 100-run.
-002200     perform 
-002300     until ws-prime-can > ws-testor ** 0.5
-002400         divide ws-testor by ws-prime-can giving ws-remainder
-002500           remainder ws-remainder
-002600         if function isPrime(ws-testor) = "Y"
-002700             display "Result: " ws-testor
-002800             goback
+001200 procedure division.
+001300 perform 100-init thru 100-exit.
+001400 goback.
+001500 100-init.
+001600     continue.
+001700 100-run.
+001800     perform 
+001900     until ws-prime-can > ws-testor ** 0.5
+002000         divide ws-testor by ws-prime-can giving ws-remainder
+002100           remainder ws-remainder
+002200         if function isPrime(ws-testor) = "Y"
+002300             display "Result: " ws-testor
+002400             goback
+002500         end-if
+002600         if ws-remainder = 0
+002700           and function isPrime(ws-prime-can) = "Y"
+002800             perform 200-prime-found
 002900         end-if
-003000         if ws-remainder = 0
-003100           and function isPrime(ws-prime-can) = "Y"
-003200             perform 200-prime-found
-003300         end-if
-003400         add 1 to ws-prime-can
-003500     end-perform.
-003600 100-exit. exit.
-003700 200-prime-found.
-003800     perform 200-append-prime.
-003900     divide ws-prime-can into ws-testor.
-004000     move 2 to ws-prime-can.
-004100 200-append-prime.
-004200     move ws-prime-can to ws-prime(i).
-004300     set i up by 1.
-004400 end program euler2.
-004500*-----------------------------------------------------------------
-004600 identification division.
-004700 function-id. isPrime.
-004800 data division.
-004900 working-storage section.
-005000     01 ws-max-divisor   pic 9(9).9(8).
-005100     01 ws-iter          pic 9(9).
-005200     01 ws-temp          pic 9(12).
-005300     01 ws-remainder     pic 9(12).
-005400 linkage section.
-005500     01 ls-test-value    pic 9(12).
-005600     01 ls-result        pic X.
-005700 procedure division
-005800     using ls-test-value
-005900     returning ls-result.
-006000 perform 100-init thru 100-exit.
-006100 goback.
-006200 100-init.
-006300     move "Y" to ls-result.
-006400     compute ws-max-divisor = ls-test-value ** 000.5 + 1.
-006500 100-test.
-006600     perform 
-006700     varying ws-iter from 2 by 1
-006800     until ws-iter > ws-max-divisor
-006900         divide ls-test-value by ws-iter giving ws-temp 
-007000           remainder ws-remainder
-007100         if ws-remainder = 0
-007200             move "N" to ls-result
-007300             goback
-007400     end-perform.
-007500 100-exit. exit.
-007600 end function isPrime.
+003000         add 1 to ws-prime-can
+003100     end-perform.
+003200 100-exit. exit.
+003300 200-prime-found.
+003400     divide ws-prime-can into ws-testor.
+003500     move 2 to ws-prime-can.
+003600 end program euler2.
+003700*-----------------------------------------------------------------
+003800 identification division.
+003900 function-id. isPrime.
+004000 data division.
+004100 working-storage section.
+004200     01 ws-max-divisor   pic 9(9).9(8).
+004300     01 ws-iter          pic 9(9).
+004400     01 ws-temp          pic 9(12).
+004500     01 ws-remainder     pic 9(12).
+004600 linkage section.
+004700     01 ls-test-value    pic 9(12).
+004800     01 ls-result        pic X.
+004900 procedure division
+005000     using ls-test-value
+005100     returning ls-result.
+005200 perform 100-init thru 100-exit.
+005300 goback.
+005400 100-init.
+005500     move "Y" to ls-result.
+005600     compute ws-max-divisor = ls-test-value ** 000.5 + 1.
+005700 100-test.
+005800     perform 
+005900     varying ws-iter from 2 by 1
+006000     until ws-iter > ws-max-divisor
+006100         divide ls-test-value by ws-iter giving ws-temp 
+006200           remainder ws-remainder
+006300         if ws-remainder = 0
+006400             move "N" to ls-result
+006500             goback
+006600     end-perform.
+006700 100-exit. exit.
+006800 end function isPrime.
