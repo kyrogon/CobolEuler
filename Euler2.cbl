eb558863ad85516440d2409d791a0c57884c353c
Aaron Rogers <aaron.kyle.rogers@gmail.com>
2022-07-06 18:25:00 -0400

    add line numbers

--- Euler2.cbl
+++ Euler2.cbl
@@ -1,28 +1,28 @@
-       identification division.
-       program-id. euler3.
-       data division.
-       working-storage section.
-           01 ws-fibs.
-               05 ws-fib   pic 9(7) occurs 3 times.
-           01 ws-limit     pic 9(7) value 4000000.
-           01 ws-result    pic 9(9) value zero.
-       procedure division.
-       perform 100-init thru 100-exit.
-       goback.
-       100-init.
-           move 1 to ws-fib(1).
-           move 1 to ws-fib(2).
-       100-run.
-           perform 200-add-next-fib until ws-fib(3) > ws-limit.
-           display "Result: " ws-result.
-       100-exit. exit.
-       200-get-next-fib.
-           add ws-fib(1) to ws-fib(2) giving ws-fib(3)
-           move ws-fib(2) to ws-fib(1).
-           move ws-fib(3) to ws-fib(2).
-       200-add-next-fib.
-           perform 200-get-next-fib.
-           if function mod(ws-fib(3), 2) = 0
-               add ws-fib(3) to ws-result
-           end-if.
-       end program euler3.
+000100 identification division.
+000200 program-id. euler3.
+000300 data division.
+000400 working-storage section.
+000500     01 ws-fibs.
+000600         05 ws-fib   pic 9(7) occurs 3 times.
+000700     01 ws-limit     pic 9(7) value 4000000.
+000800     01 ws-result    pic 9(9) value zero.
+000900 procedure division.
+001000 perform 100-init thru 100-exit.
+001100 goback.
+001200 100-init.
+001300     move 1 to ws-fib(1).
+001400     move 1 to ws-fib(2).
+001500 100-run.
+001600     perform 200-add-next-fib until ws-fib(3) > ws-limit.
+001700     display "Result: " ws-result.
+001800 100-exit. exit.
+001900 200-get-next-fib.
+002000     add ws-fib(1) to ws-fib(2) giving ws-fib(3)
+002100     move ws-fib(2) to ws-fib(1).
+002200     move ws-fib(3) to ws-fib(2).
+002300 200-add-next-fib.
+002400     perform 200-get-next-fib.
+002500     if function mod(ws-fib(3), 2) = 0
+002600         add ws-fib(3) to ws-result
+002700     end-if.
+002800 end program euler3.
--- Euler48.cbl
+++ Euler48.cbl
@@ -1,29 +1,28 @@
-       identification division.
-       program-id. euler48.
-       data division.
-       working-storage section.
-           01 ws-power     pic 9(10).
-           01 ws-result    pic 9(10).
-           01 ws-limit     pic 9(4) value 1000.
-           01 ws-current   pic 9(4) value 999.
-           01 ws-x         pic 9(10).
-
-       procedure division.
-       100-init.
-           accept ws-limit from command-line.
-       100-run.
-           perform 
-           varying ws-current from 1 by 1
-           until ws-current > ws-limit
-               perform 200-calc-power
-               add ws-power to ws-result
-           end-perform.
-           display ws-result.
-       100-exit. exit.
-       goback.
-       200-calc-power.
-           move 1 to ws-power.
-           perform varying ws-x from 1 by 1 until ws-x > ws-current
-               multiply ws-current by ws-power
-           end-perform.
-       end program euler48.
+000100 identification division.
+000200 program-id. euler48.
+000300 data division.
+000400 working-storage section.
+000500     01 ws-power     pic 9(10).
+000600     01 ws-result    pic 9(10).
+000700     01 ws-limit     pic 9(4) value 1000.
+000800     01 ws-current   pic 9(4) value 999.
+000900     01 ws-x         pic 9(10).
+001000 procedure division.
+001100 100-init.
+001200     accept ws-limit from command-line.
+001300 100-run.
+001400     perform 
+001500     varying ws-current from 1 by 1
+001600     until ws-current > ws-limit
+001700         perform 200-calc-power
+001800         add ws-power to ws-result
+001900     end-perform.
+002000     display ws-result.
+002100 100-exit. exit.
+002200 goback.
+002300 200-calc-power.
+002400     move 1 to ws-power.
+002500     perform varying ws-x from 1 by 1 until ws-x > ws-current
+002600         multiply ws-current by ws-power
+002700     end-perform.
+002800 end program euler48.
