8c1aa674d04aae643375cfe47c58783153e2ea5e
Aaron Rogers <aaron.kyle.rogers@gmail.com>
2022-07-06 18:36:16 -0400

    update format

--- Euler48.cbl
+++ Euler48.cbl
@@ -4,12 +4,12 @@
 000400 working-storage section.
 000500     01 ws-power     pic 9(10).
 000600     01 ws-result    pic 9(10).
-000700     01 ws-limit     pic 9(4) value 1000.
-000800     01 ws-current   pic 9(4) value 999.
+000700     01 ws-current   pic 9(4).
+000800     01 ws-limit     pic 9(4) value 1000.
 000900     01 ws-x         pic 9(10).
 001000 procedure division.
-001100 100-init.
-001200     accept ws-limit from command-line.
+001100 perform 100-run thru 100-exit.
+001200 goback.
 001300 100-run.
 001400     perform 
 001500     varying ws-current from 1 by 1
@@ -19,10 +19,9 @@
 001900     end-perform.
 002000     display ws-result.
 002100 100-exit. exit.
-002200 goback.
-002300 200-calc-power.
-002400     move 1 to ws-power.
-002500     perform varying ws-x from 1 by 1 until ws-x > ws-current
-002600         multiply ws-current by ws-power
-002700     end-perform.
-002800 end program euler48.
+002200 200-calc-power.
+002300     move 1 to ws-power.
+002400     perform varying ws-x from 1 by 1 until ws-x > ws-current
+002500         multiply ws-current by ws-power
+002600     end-perform.
+002700 end program euler48.
