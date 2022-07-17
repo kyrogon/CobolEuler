dc500aebe3ca23e87f2a5d050dafbd85c5ae2185
kyrogon <aaron.kyle.rogers@gmail.com>
2022-07-17 18:54:58 -0400

    Make function sumDivisibleBy more flexible

--- Euler1.cbl
+++ Euler1.cbl
@@ -6,28 +6,28 @@
 000600     function sumDivisibleBy.
 000700 data division.
 000800 working-storage section.
-000900     01 ws-maximum   pic 9(4)    value 1000.
-001000     01 ws-multiple  pic 9(2)    value 3.
-001100     01 ws-result    pic 9(6)    value 0.
-001200 procedure division.
-001300 perform 100-run thru 100-exit.
-001400 goback.
-001500 100-run.
-001600     compute ws-result = sumDivisibleBy(03, 1000)
-001700         +sumDivisibleBy(05, 1000)
-001800         -sumDivisibleBy(15, 1000).
-001900     display "Result: " ws-result.
-002000 100-exit. exit.
-002100 end program euler1.
-002200*-----------------------------------------------------------------
-002300 identification division.
-002400 function-id. sumDivisibleBy.
-002500 data division.
-002600 working-storage section.
-002700     01 ws-current   pic 9(4).
+000900     01 ws-result    pic 9(6)    value 0.
+001000 procedure division.
+001100 perform 100-run thru 100-exit.
+001200 goback.
+001300 100-run.
+001400     compute ws-result = sumDivisibleBy(3, 1000)
+001500         +sumDivisibleBy(5, 1000)
+001600         -sumDivisibleBy(15, 1000).
+001700     display "Result: " ws-result.
+001800 100-exit. exit.
+001900 end program euler1.
+002000*-----------------------------------------------------------------
+002100 identification division.
+002200 function-id. sumDivisibleBy.
+002300 data division.
+002400 working-storage section.
+002500     01 ws-current   pic 9(4).
+002600     01 ws-multiple  pic 9(4).
+002700     01 ws-maximum   pic 9(4).
 002800 linkage section.
-002900     01 ls-multiple  pic 9(2).
-003000     01 ls-maximum   pic 9(4).
+002900     01 ls-multiple  pic X any length.
+003000     01 ls-maximum   pic X any length.
 003100     01 ls-result    pic 9(6).
 003200 procedure division
 003300 using ls-multiple ls-maximum
@@ -35,13 +35,15 @@
 003500 perform 100-init thru 100-exit.
 003600 goback.
 003700 100-init.
-003800     move 0 to ls-result.
-003900 100-run.
-004000     perform
-004100     varying ws-current from 0 by ls-multiple
-004200     until ws-current >= ls-maximum
-004300         add ws-current to ls-result
-004400     end-perform.
-004500 100-exit. exit.
-004600 end function sumDivisibleBy.
-004700 
+003800     move ls-multiple to ws-multiple.
+003900     move ls-maximum to ws-maximum.
+004000     move 0 to ls-result.
+004100 100-run.
+004200     perform
+004300     varying ws-current from 0 by ws-multiple
+004400     until ws-current >= ws-maximum
+004500         add ws-current to ls-result
+004600     end-perform.
+004700 100-exit. exit.
+004800 end function sumDivisibleBy.
+004900 
